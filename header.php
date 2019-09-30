03ca2cba791faf69f48d899fb9fd4d419b755569
Dincius <diana.meksriunaite@gmail.com>
2019-09-30 21:23:45 +0300

    Pabandymas :)

---
+++ header.php
@@ -0,0 +1,26 @@
+
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="marvel.php">Marvel</a></li>
+  <li><a href="dc.php">DC</a></li>
+  <li class="divider"></li>
+</ul>
+<nav>
+  <div class="parallax-container nav-wrapper cyan">
+    <a href="#!" class="brand-logo center"><img src="images/logo.png" width=50px></a>
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="index.html">Pagrindinis</a></li>
+      <li><a href="naujienos.html">Naujienos</a></li>
+      <li><a href="roko.html">Premjeros</a></li>
+      <li><a href="bendras.html">Statistika</a></li>
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-trigger" href="brigitos.php" data-target="dropdown1">Kompanijos<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
+<ul class="sidenav cyan" id="mobile-demo">
+    <li><a href="index.php">Pagrindinis</a></li>
+    <li><a href="naujienos.php">Naujienos</a></li>
+    <li><a href="roko.php">Premjeros</a></li>
+    <li><a href="brigitos.php">Kompanijos</a></li>
+  </ul>
