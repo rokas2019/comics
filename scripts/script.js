b0bed59a29ae48bf2836cbf6c56870611844368e
kamija94 <kamile.janusauskaite@gmail.com>
2019-10-01 21:25:44 +0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -7,18 +7,6 @@ $(document).ready(function(){
   });
 
 
-$('input[type=radio]').change(function() {
-
-	if (this.value =='batman') {
-
-		alert("You're right!");
-	}
-
-	else {
-		alert ('Not quite right, take a look at the "News" section ' );
-		}
-
-	});
 
 
 
