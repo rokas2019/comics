ad5e47e0b7ea38bf4edd25c610315a6963c61c1d
Dincius <diana.meksriunaite@gmail.com>
2019-10-01 21:21:25 +0300

    Mano skiltis

---
+++ naujienos.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Naujienos</title>
+	<?php include "head.php"; ?>
+</head>
+<body>
+	
+  <?php include "header.php"; ?>
+
+
+  <div class="container">
+    <h1 class="cyan-text center-align">Comics news</h1>
+    <ul class="collection z-depth-4">
+
+      <a class="modal-trigger" href="#modal2">
+        <li class="collection-item avatar">
+          <img src="images/babybat.jpg" alt="Baby-Bat" class="circle">
+          <span class="title indigo-text">The best facepalms of Baby-Bat</span>
+          <p>He is vengeance. He is the night. He is...Bat-Baby?! <br>
+           Have you ever looked at your old high school yearbook and wanted to bury it somewhere, or do you wish you could run and hide when a really embarrassing memory shows up on your Timehop? 
+         </p>
+       </a>
+     </li>
+
+     <div id="modal2" class="modal">
+      <div class="modal-content">
+        <h3 class="cyan-text center-align">Bat-Baby: Five of the Bat Family's Best Facepalms</h3>
+        <p class="pirma center-align">He is vengeance. He is the night. He is...Bat-Baby?!</p>
+        <p class="center-align">
+          Have you ever looked at your old high school yearbook and wanted to bury it somewhere, or do you wish you could run and hide when a really embarrassing memory shows up on your Timehop? We all have those humiliating moments and the Batman family is no exception. When you’ve been around for 80 years there’s bound to be a bit of cringe along the way. Not every story can be The Dark Knight Returns or Hush, sometimes you get “Batman Becomes Bat-Baby.” This Batman Day, let’s celebrate and honor the Dark Knight the same way we do with all of our best friends—by telling embarrassing stories about their past! Here are a few of the less stellar moments he and his allies have had throughout their rich and diverse history.
+        </p>
+
+<div class="gallery">
+    <img class="materialboxed" src="images/Batman1.jpg" alt="Baby-Bat" height="200px">
+  <div class="desc">The Time Bruce Wayne Was Caught Cheating and Said Batman Made Him Do It</div>
+</div>
+
+<div class="gallery">
+    <img class="materialboxed" src="images/Batman2.jpg" alt="Baby-Bat2" height="200px">
+  <div class="desc">The Time Alfred Spanked Catwoman</div>
+</div>
+
+<div class="gallery">
+     <img class="materialboxed" src="images/Batman3.jpg" alt="Baby-bat3Northern Lights" height="200px">
+   <div class="desc">The Time Gordon Vowed to Keep Batman’s Identity a Secret…and Totally Didn’t</div>
+</div>
+
+<div class="gallery">
+     <img class="materialboxed" src="images/Batman4.jpg" height="200px">
+   <div class="desc">The Time Batman Turned Into a Bat-Baby and Just Went with It</div>
+</div>
+
+<div class="gallery">
+     <img class="materialboxed" src="images/Batman5.jpg" height="200px">
+  <div class="desc">The Time Batman Reminded Us That When You’ve Gotta Go, You’ve Gotta Go</div>
+</div>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Thanks for the news!</a>
+      </div>
+    </div>
+
+    <a class="modal-trigger" href="#modal1">
+      <li class="collection-item avatar">
+        <img src="images/Batman 15.jpg" class="circle">
+        <span class="title indigo-text">Batman's 80th anniversary!</span>
+        <p>The Bat-Signal has appeared over the Island.  <br>
+          For 80 years, the Dark Knight has stood as a symbol of determination, bravery and justice to generations of fans. Since appearing in the pages of DETECTIVE COMICS #27 on March 30, 1939, Batman continues to leave his mark in every form of entertainment imaginable.
+        </p> 
+      </li>
+    </a>
+
+
+    <div id="modal1" class="modal">
+      <div class="modal-content">
+        <h3 class="cyan-text center-align">Batman's 80th aniversary!</h3>
+        <p>For 80 years, the Dark Knight has stood as a symbol of determination, bravery and justice to generations of fans. Since appearing in the pages of DETECTIVE COMICS #27 on March 30, 1939, Batman continues to leave his mark in every form of entertainment imaginable. Appearing in countless comics, animation, television, films, video games, toys and other products, he remains the single most popular superhero. He would not be complete without the most iconic gadgets and of course, the Batmobile; not to mention, an unparalleled rogues gallery.
+
+          Batman is humanity’s timeless hero and he’s just getting started.
+
+        Join us for a year-long global celebration of the Caped Crusader! #LongLiveTheBat #Batman80 #Detective1000</p>
+        <p>Batman Day is almost here and to celebrate the Dark Knight’s 80 years as the World’s Greatest Detective, DC and Warner Bros. are lighting up the night skies by projecting the iconic Bat-Signal, as well as producing fan celebrations, in major cities around the globe on Saturday, September 21.
+
+          Scroll and zoom your way around the world in the map below for more information on these global celebrations!
+        </p>
+
+        <p class="cyan-text center-align">Batman 80 Playlist</p>
+        <div class="video-container">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/Gm5zjxSrx6g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Thanks for the news!</a>
+      </div>
+    </div>
+
+
+    <a href="news3.php" target="_blanck">
+      <li class="collection-item avatar">
+        <img src="images/superman.png" class="circle">
+        <span class="title indigo-text">Super Man</span>
+        <p>Faster than a speeding bullet, more powerful than a locomotive… <br>
+         From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+       </p>
+     </a>
+   </li>
+
+
+   <a href="news4.php" target="_blanck">
+    <li class="collection-item avatar">
+      <img src="images/spiderham.jpg" class="circle">
+      <span class="title indigo-text">Spider-Ham is back!</span>
+      <p>New Peter Porker Short Has Spider-Ham Fighting Super Villains and Eating Hot Dogs<br>
+        Though his hands might always be a little bit wet (it's because he just washed them), we still can't get enough of Peter Porker from Spider-Man: Into the Spider-Verse.
+      </p>
+    </a>
+  </li>
+</ul>
+
+<?php include "footer.php"; ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript" src="scripts/script.js"></script>
+
+</body>
+</html>
