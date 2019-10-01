c127129e0ac7402c6bdf0b0cff2a708930acaf84
kamija94 <kamile.janusauskaite@gmail.com>
2019-10-01 21:24:42 +0300

    index update

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
      $('.parallax').parallax();
      $('.sidenav').sidenav();
   });
+
+
+$('input[type=radio]').change(function() {
+
+	if (this.value =='batman') {
+
+		alert("You're right!");
+	}
+
+	else {
+		alert ('Not quite right, take a look at the "News" section ' );
+		}
+
+	});
+
+
+
