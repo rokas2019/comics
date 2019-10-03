9a0a4c446cd3672396a47060c16ce0ff8ac6c68f
Brigita08r <brigita08r@gmail.com>
2019-10-03 21:18:06 +0300

    pakeiciau linkus i jusu puslapius

--- companies.php
+++ companies.php
@@ -26,10 +26,10 @@
      <a href="#!" class="brand-logo center"><img src="images/logo.png" width=50px></a>
      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">Menu</i></a>
     <ul class="right hide-on-med-and-down">
-      <li><a href="index.html">Main Page</a></li>
-      <li><a href="naujienos.html">News</a></li>
-      <li><a href="roko.html">Premieres</a></li>
-      <li><a href="bendras.html">Statistics</a></li>
+      <li><a href="index.php">Main Page</a></li>
+      <li><a href="naujienos.php">News</a></li>
+      <li><a href="news.php">Premieres</a></li>
+      <li><a href="statistics.php">Statistics</a></li>
       <!-- Dropdown Trigger -->
       <li><a class="dropdown-trigger" href="index4.php" data-target="dropdown1">Companies<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
@@ -39,8 +39,8 @@
   <ul class="sidenav cyan" id="mobile-demo">
     <li><a href="index.php">Main Page</a></li>
     <li><a href="naujienos.php">News</a></li>
-    <li><a href="roko.php">Premieres</a></li>
-    <li><a href="index4.php">Companies</a></li>
+    <li><a href="news.php">Premieres</a></li>
+    <li><a href="Companies.php">Companies</a></li>
   </ul>
 
 </head>
