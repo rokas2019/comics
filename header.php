d9f0f858e7af6f32a385c10f85b95cb01f9625ee
kamija94 <kamile.janusauskaite@gmail.com>
2019-10-04 20:33:08 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <ul id="dropdown1" class="dropdown-content">
   <li><a href="companies.php">Marvel</a></li>
-
   <li><a href="#!tab-3">DC</a></li>
-
   <li class="divider"></li>
+  <li><a href="#!">Universal pictures</a></li>
+  <li><a href="#!">Image comics</a></li>
 </ul>
+
 <nav>
-  <div class="parallax-container nav-wrapper cyan">
-    <a href="index.php" class="brand-logo center"><img src="images/logo.png" width=50px></a>
-    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">Menu</i></a>
-    <ul class="right hide-on-med-and-down">
+  <div class="nav-wrapper cyan">
+   <a href="index.php" class="brand-logo center"><img src="images/logo.png" width=50px></a>
+     <ul class="right hide-on-med-and-down">
       <li><a href="index.php">Main Page</a></li>
       <li><a href="naujienos.php">News</a></li>
       <li><a href="news.php">Premieres</a></li>
@@ -18,12 +18,13 @@
       <!-- Dropdown Trigger -->
       <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Companies<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
-  </div>
-</nav>
+  <
 <ul class="sidenav cyan" id="mobile-demo">
     <li><a href="index.php">Main page</a></li>
     <li><a href="naujienos.php">News</a></li>
     <li><a href="news.php">Premieres</a></li>
     <li><a href="companies.php">Companies</a></li>
     <li><a href="volume.php">Comics</a></li>
-  </ul>
+</ul>
+</div>
+</nav>
