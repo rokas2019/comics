b153d0d41c9242e78be0fa6d39930572a81f99a5
Dincius <diana.meksriunaite@gmail.com>
2019-10-04 21:34:03 +0300

    pakeista

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <nav>
   <div class="nav-wrapper cyan">
    <a href="index.php" class="brand-logo center"><img src="images/logo.png" width=50px></a>
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons dehaze">dehaze</i></a>
      <ul class="right hide-on-med-and-down">
       <li><a href="index.php">Main Page</a></li>
       <li><a href="naujienos.php">News</a></li>
@@ -18,7 +19,9 @@
       <!-- Dropdown Trigger -->
       <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Companies<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
-  <
+</div>
+
+</nav>  
 <ul class="sidenav cyan" id="mobile-demo">
     <li><a href="index.php">Main page</a></li>
     <li><a href="naujienos.php">News</a></li>
@@ -26,8 +29,6 @@
     <li><a href="companies.php">Companies</a></li>
     <li><a href="volume.php">Comics</a></li>
 </ul>
-</div>
 
-</nav>
 
 
